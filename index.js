35b7a21c666bf2113587010c29d76c6a64cd4742
Lohitha-Kakumani <kakumanilohitha2005@gmail.com>
2024-12-10 21:07:58 +0530

    first repo

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv"; // to keep an sensitive data
+import cors from "cors";
+
+
+const app = express();
